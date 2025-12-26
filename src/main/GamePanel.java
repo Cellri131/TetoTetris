615f44fca13a836a7539fd9067f5ebba5ed0c6c8
Romain <r_baruchello@hotmail.com>
2025-12-26 15:14:39 +0100

    ajout de la gameLoop dans GamePanel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.Color;
 import javax.swing.JPanel;
+import java.awt.Graphics;
 
 public class GamePanel extends JPanel implements Runnable {
     
@@ -18,9 +19,42 @@ public class GamePanel extends JPanel implements Runnable {
         this.setLayout(null);
     }
 
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
 
     public void run() {
+        
+        //Game loop
+        double drawInterval = 1000000000 / FPS; // 0.01666667 secondes
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    public void update() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // TODO Auto-generated method stub
+        
     }
 }
