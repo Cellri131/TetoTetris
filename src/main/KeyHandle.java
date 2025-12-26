78809cc11d8e2ebfe6cf01fde12ff000a434e33d
Romain <r_baruchello@hotmail.com>
2025-12-26 23:30:07 +0100

    ajout de la lecture des différentes touches de déplacement (ZQSD)

--- src/main/KeyHandle.java
+++ src/main/KeyHandle.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyListener;
 public class KeyHandle implements KeyListener{
 
 
-    public static boolean upPressed, downPressed, leftPressed, rightPressed;
+    public static boolean upPressed, leftPressed, downPressed, rightPressed;
 
     @Override
     public void keyTyped(KeyEvent e) {}
@@ -16,9 +16,17 @@ public class KeyHandle implements KeyListener{
         
         int code = e.getKeyCode();
 
-    if(code == KeyEvent.VK_Z)
-        upPressed = true;
-        
+        if(code == KeyEvent.VK_Z)
+            upPressed    = true;
+    
+        if(code == KeyEvent.VK_Q)
+            leftPressed  = true;
+
+        if(code == KeyEvent.VK_S)
+            rightPressed = true;
+
+        if(code == KeyEvent.VK_D)
+            downPressed  = true;
     }
 
     @Override
