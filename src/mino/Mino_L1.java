8ac15a85784f802b15296155537168873acfdbf2
Romain <r_baruchello@hotmail.com>
2025-12-26 19:06:56 +0100

    initialisation du block L1

--- src/mino/Mino_L1.java
+++ src/mino/Mino_L1.java
@@ -9,12 +9,21 @@ public class Mino_L1 extends Mino {
     }
 
     public void setXY(int x, int y){
-        // []
-        // [] <- point de rotation donc point d'orig soit le point b[0]
-        // [][]
+        // [1]
+        // [0] <- point de rotation donc point d'orig soit le point b[0]
+        // [2][3]
 
         b[0].x = x;
         b[0].y = y;
+        
         b[1].x = b[0].x;
+        b[1].y = b[0].y - Block.SIZE;
+
+        b[2].x = b[0].x;
+        b[2].y = b[0].y + Block.SIZE;
+
+        b[3].x = b[0].x + Block.SIZE;
+        b[3].y = b[0].y + Block.SIZE;
+
     }
 }
